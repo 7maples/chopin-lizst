5151fdae08f040808031ec844cb1eff1104fe1d0
Jennifer Eliuk <jen7maples@gmail.com>
2014-11-25 13:52:57 -0700

    add additional routes

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,12 +2,43 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req]
  {:status 200
   :body "Hello, beautiful!"
   :headers {}})
+
+(defn about [req]
+  {:status 200
+   :body "A shopping list in Clojure"
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status 200
+     :body (str "Yo " name "!")
+     :headers {}}))
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body (str "Unknown operator: " op)
+       :headers {}})))
 
 (defn goodbye [req]
   {:status 200
@@ -16,7 +47,11 @@
 
 (defroutes app
   (GET "/" [] greet)
+  (GET "/about" [] about)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo)
+  (GET "/calc/:a/:op/:b" [] calc)
   (not-found "Page not found."))
 
 (defn -main [port]
